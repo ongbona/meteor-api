bbcad6b3eb9630c872e74b5be4c26a625b6d2e4c
BonaOng <bonasmart9@gmail.com>
2019-03-26 07:46:54 +0700

    on backend file

--- both/convertToResAPI.js
+++ both/convertToResAPI.js
@@ -15,4 +15,22 @@ JsonRoutes.add('get', '/find_customer/:selector/:options', function (
     data: data,
   })
 })
+JsonRoutes.add('post', '/insert_customer', function (req, res, next) {
+  res.charset = 'utf-8'
+  const doc = req.body
+  StudentM.insertStudent(doc, (error, result) => {
+    let data = {}
+    if (error) {
+      data.code = '403'
+      data.msg = error.message
+      data.result = ''
+    } else {
+      data.code = '201'
+      data.result = result
+    }
+    JsonRoutes.sendResult(res, {
+      data: data,
+    })
+  })
+})
 // របៀបconvert method to api
